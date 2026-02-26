a5a8649213c311495d1e82357e2ff73ef51e747d
shiv578 <swarajrkfl22@gmail.com>
2026-02-26 20:53:55 +0530

    removed the api

--- node/googleFit/googleFitConfig.js
+++ node/googleFit/googleFitConfig.js
@@ -1,13 +1,1 @@
-module.exports = {
-  client_id: "269984522890-uvhcb0nr954av1ettntf3e13enbgl24g.apps.googleusercontent.com",
-  client_secret: "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",
-
-  redirect_uri: "https://fitnationproject.vercel.app/auth/callback",
-
-  scopes: [
-    "https://www.googleapis.com/auth/fitness.activity.read",
-    "https://www.googleapis.com/auth/fitness.body.read",
-    "https://www.googleapis.com/auth/fitness.location.read",
-    "https://www.googleapis.com/auth/fitness.heart_rate.read"
-  ]
-};
+//
