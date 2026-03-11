e86647bc4b5ac070c6f83e285f4cd329d83b7f02
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-03-11 21:04:35 +0530

    Update DailyChallenge.jsx

--- react/src/components/DailyChallenge.jsx
+++ react/src/components/DailyChallenge.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
-import css from "./DailyChallenge.module.css";
+ import css from "./DailyChallenge.module.css";
 
  const API_BASE = import.meta.env.VITE_API_URL;
 
