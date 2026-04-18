adbefaf43b035698ac53a4cffe0fbeb8cc576aff
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-04-18 22:52:47 +0530

    Fix import statement formatting in App.jsx

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,4 +1,4 @@
-     import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import SignIn from "./SignIn.jsx";
 import Createaccount from "./createaccount.jsx";
 import Dashboard from "./Dashboard.jsx";
