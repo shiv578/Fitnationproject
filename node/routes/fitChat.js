5e351e265b1ced2a98286ea196d6766eb845e5bf
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-04-23 22:28:38 +0530

    Update fitChat.js

--- node/routes/fitChat.js
+++ node/routes/fitChat.js
@@ -1,5 +1,5 @@
    const express = require("express");
-const router = express.Router();
+ const router = express.Router();
 const axios = require("axios");
 
 router.post("/ask", async (req, res) => {
