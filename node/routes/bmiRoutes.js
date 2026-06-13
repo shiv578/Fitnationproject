d14e85b0c3831ee2973022e5fdb1da458c0dace5
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-06-13 15:52:39 +0530

    Update bmiRoutes.js

--- node/routes/bmiRoutes.js
+++ node/routes/bmiRoutes.js
@@ -1,4 +1,4 @@
-const express = require("express");
+ const express = require("express");
 const BMIHistory = require("../models/BMIHistory");
 const { getBMICategory } = require("../utils/bmiCategory");
 
