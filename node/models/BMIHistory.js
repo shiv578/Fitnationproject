0c801a22440212985649b675a124b50dcd614941
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-06-22 09:00:48 +0530

    Fix formatting of BMIHistory.js schema definition

--- node/models/BMIHistory.js
+++ node/models/BMIHistory.js
@@ -1,6 +1,6 @@
         const mongoose = require("mongoose");
 
-const bmiHistorySchema = new mongoose.Schema({
+const bmiHistorySchema = new mongoose.Schema({ 
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
