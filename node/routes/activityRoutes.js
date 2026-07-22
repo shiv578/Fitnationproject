cdeae092acdc3ac5b0ba875ca4843fe1c5e26d34
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-07-22 16:56:59 +0530

    Fix formatting in activityRoutes.js

--- node/routes/activityRoutes.js
+++ node/routes/activityRoutes.js
@@ -1,7 +1,7 @@
       const firebaseConfig = {
    apiKey: "AIzaSyDAVx8IEGn25XWAG_Q6AWV7e3n12aepgLQ",
   authDomain: "fitnation-5bf70.firebaseapp.com", 
-  projectId: "fitnation-5bf70",
+  projectId: "fitnation-5bf70", 
   storageBucket: "fitnation-5bf70.firebasestorage.app",
   messagingSenderId: "208973525620",
   appId: "1:208973525620:web:894a154edfab349d649721",
