28f69d77fd44f2626ec06d633fb0fd1996b1a137
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-07-29 22:44:01 +0530

    Update streakTasks.js

--- node/utils/streakTasks.js
+++ node/utils/streakTasks.js
@@ -6,6 +6,7 @@ const TASKS = [
   
   
   { level: "Easy", text: "Walk 8,000 steps" },
+  
   { level: "Easy", text: "Do 50 squats" },
   { level: "Easy", text: "Do 50 push-ups" },
   { level: "Easy", text: "Stretch for 20 minutes" },
