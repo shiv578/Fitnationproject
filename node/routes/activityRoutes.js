c138d7030e166ee9b21121c911ce6918878305e6
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-07-31 10:08:05 +0530

    Update activityRoutes.js

--- node/routes/activityRoutes.js
+++ node/routes/activityRoutes.js
@@ -3,7 +3,7 @@
   authDomain: "fitnation-5bf70.firebaseapp.com", 
   projectId: "fitnation-5bf70", 
   storageBucket: "fitnation-5bf70.firebasestorage.app", 
-  messagingSenderId: "208973525620", 
+  messagingSenderId: "208973525620",  
   appId: "1:208973525620:web:894a154edfab349d649721",  
   measurementId: "G-4J4KFS7RYZ" 
 };   
