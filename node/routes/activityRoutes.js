7cf32c330ec7716da208bb947292ec6556094fa0
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-07-31 10:08:26 +0530

    Update activityRoutes.js

--- node/routes/activityRoutes.js
+++ node/routes/activityRoutes.js
@@ -4,7 +4,7 @@
   projectId: "fitnation-5bf70", 
   storageBucket: "fitnation-5bf70.firebasestorage.app", 
   messagingSenderId: "208973525620",  
-  appId: "1:208973525620:web:894a154edfab349d649721",   
+  appId: "1:208973525620:web:894a154edfab349d649721",    
   measurementId: "G-4J4KFS7RYZ"  
 };   
  
