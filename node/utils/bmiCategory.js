b637d9b2412264f5c6a2614027e429881fc4e464
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-08-05 11:54:51 +0530

    Update bmiCategory.js

--- node/utils/bmiCategory.js
+++ node/utils/bmiCategory.js
@@ -1,4 +1,5 @@
             function getBMICategory(bmi) {
+                        
   if (bmi < 18.5) return "Underweight";
   if (bmi >= 18.5 && bmi < 25) return "Fit";   
   if (bmi >= 25 && bmi < 30) return "Overweight" ;
