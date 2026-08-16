c9ce073eb242399da5e606aae7aaa19847f53b90
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-08-16 20:51:33 +0530

    Update bmiRoutes.js

--- node/routes/bmiRoutes.js
+++ node/routes/bmiRoutes.js
@@ -3,7 +3,8 @@ const BMIHistory = require("../models/BMIHistory");
 const { getBMICategory } = require("../utils/bmiCategory"); 
   
 const router = express.Router();
-   
+
+
            /**   
    *   SAVE BMI    
  */
