fa0b69067bd6a6264c69670eb69f5109d54539ba
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-08-20 23:21:25 +0530

    Clean up blank lines in challengeRoutes.js
    Removed unnecessary blank lines in challengeRoutes.js

--- node/routes/challengeRoutes.js
+++ node/routes/challengeRoutes.js
@@ -3,9 +3,6 @@ const router = express.Router();
 const UserStreak = require("../models/UserStreak");
 const { getTaskForDay } = require("../utils/streakTasks");
 
-
-
-
 // YYYY-MM-DD (local) 
 const todayStr = () => {   
   const d = new Date();
