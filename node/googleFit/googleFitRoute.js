f8477b2d6820dd5a5d01bd7029cb3f67b9b979ff
Shivam Swaraj <swarajrkfl22@gmail.com>
2026-08-23 20:46:28 +0530

    Update googleFitRoute.js

--- node/googleFit/googleFitRoute.js
+++ node/googleFit/googleFitRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const axios = require("axios");
 const qs = require("qs");
 const { client_id, client_secret, redirect_uri, scopes } = require("./googleFitConfig");
-
+ 
        // -------------  -----------------
  // STEP 1 → GOOGLE LOGIN
 // ------------------------------
